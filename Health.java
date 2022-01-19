0a710e3c9778460ee4c7eb703ebe10c0acde995c
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 03:58:43 +0900

    Health.java

--- Health.java
+++ Health.java
@@ -1,21 +1,22 @@
 class Health {
     public double height, weight;
 
-	//課題4
-	//身長と体重のデータの更新
+    //課題3
+    //身長と体重のデータの更新
     public void update(double height, double weight) {
-
+        this.height = height;
+        this.weight = weight;
     }
 
-	//課題4
-	//BMIの計算結果を返す
+    //課題3
+    //BMIの計算結果を返す
     public double bmi() {
-        return 0;
+        return weight / height / height;
     }
 
-	//課題4
-	//BMIが標準値であればtrue,標準値でなければfalseを返す
+    //課題3
+    //BMIが標準値であればtrue,標準値でなければfalseを返す
     public boolean isHealthy() {
-
+        return 18.5 <= bmi() && bmi() < 25.0;
     }
 }
